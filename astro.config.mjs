71b0944c35635f607474b7732b74b66868da0c4c
Luis M. Rosso <lrosso@santandertecnologia.com.ar>
2024-08-07 23:38:31 -0300

    tracker 6: Back on dry land

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-blog-lmr.netlify.app"
+  site: "https://astro-blog-lmr.netlify.app",
+  integrations: [preact()]
 });
